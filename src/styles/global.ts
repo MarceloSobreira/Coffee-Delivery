4654d7dce9446e2acbaa186d24ea48b9ca2f623f
Marcelo S <marcelo15011@gmail.com>
2022-09-01 13:33:27 -0300

    Configurações inicias para o funcionamento do projeto.

---
+++ src/styles/global.ts
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :focus {
+    outline: 0;
+  }
+
+  body {
+    background: ${(props) => props.theme['base-background']};
+    color: ${(props) => props.theme['base-text']};
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400 ;
+    font-size: 1rem;
+  }
+
+  h1, h2, h3, strong {
+    font-family: 'Baloo 2', cursive;
+    font-weight: 800;
+  }
+`
